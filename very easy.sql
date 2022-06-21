27dcde08229fb47da15911f45ec95a1b3754d52f
EthanDoan3655 <37298042+EthanDoan3655@users.noreply.github.com>
2022-06-20 20:37:57 -0400

    Add files via upload

---
+++ very easy.sql
@@ -0,0 +1,31 @@
+SHOW DATABASES;
+CREATE DATABASE guided_inquiry;
+SHOW DATABASES;
+USE guided_inquiry;
+CREATE TABLE cars(
+id INT AUTO_INCREMENT PRIMARY KEY,
+make VARCHAR(30),
+model VARCHAR(30),
+year YEAR
+);
+
+SHOW TABLES;
+
+USE guided_inquiry;
+
+SELECT*FROM cars;
+
+INSERT INTO very_easy (make,model,year)
+VALUES ('Toyota','Camry',2001);
+
+INSERT INTO very_easy (make,model,year)
+VALUES ('Mazda','Rx7',1994);
+
+INSERT INTO very_easy (make,model,year)
+VALUES ('BMW','E36',1994);
+
+INSERT INTO very_easy (make,model,year)
+VALUES ('Honda','s200',2005);
+
+INSERT INTO very_easy (make,model,year)
+VALUES ('Subaru','WRX',2004);
